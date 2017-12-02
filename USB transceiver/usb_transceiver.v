1d01af535cfd2c9f47e8d49b68cc68580af02eab
RakhDamir <rakhimovdamir05@gmail.com>
2017-12-02 16:08:38 +0300

    Update and rename usb_transceiver.v to USB transceiver/usb_transceiver.v

--- USB transceiver/usb_transceiver.v
+++ USB transceiver/usb_transceiver.v
@@ -1,27 +1,27 @@
 module usb_transceiver(
-			input 			      rst,
-			input				      clk,
-			input				      rx_ready,
-			input						tx_ready,
+			input 			    rst,
+			input				clk,
+			input				rx_ready,
+			input				tx_ready,
 			output	reg			wr_en,
-			output	reg	      rd_en,
-			inout		      [7:0]	databus
+			output	reg	        rd_en,
+			inout		 [7:0]	databus
 			);
 
-parameter RD_IDLE = 3'b000;
-parameter RD_PRE_WAIT = 3'b001;
-parameter RD_POST_WAIT = 3'b010;
-parameter WR_IDLE = 3'b011;
-parameter WR_WAIT = 3'b100;
-parameter WR_DONE = 3'b101;
+parameter 	RD_IDLE = 3'b000;
+parameter 	RD_PRE_WAIT = 3'b001;
+parameter 	RD_POST_WAIT = 3'b010;
+parameter 	WR_IDLE = 3'b011;
+parameter 	WR_WAIT = 3'b100;
+parameter 	WR_DONE = 3'b101;
 
-reg [2:0] state = RD_IDLE;
-reg [3:0] cnt = 4'b0000;
+reg [2:0] 	state = RD_IDLE;
+reg [3:0] 	cnt = 4'b0000;
 
 // bidirectional bus
-reg [7:0] data_rx = 8'b0000_0000;
-reg [7:0] data_tx;
-reg 		 out_en;
+reg [7:0] 	data_rx = 8'b0000_0000;
+reg [7:0] 	data_tx;
+reg 		out_en;
 
 
 assign databus = out_en ? data_tx : 8'bZZZZ_ZZZZ;
@@ -54,28 +54,28 @@ always @ (posedge clk)
 				end
 			end
 			RD_POST_WAIT: begin
-				rd_en <= 1'b1;
-				cnt <= cnt + 1'b1;
+				rd_en 	<= 1'b1;
+				cnt 	<= cnt + 1'b1;
 				if (cnt == 4'b0001) begin
-					state <= WR_IDLE;
-					cnt   <= 4'b0000;
-					out_en <= 1'b0;
+					state 	<= WR_IDLE;
+					cnt   	<= 4'b0000;
+					out_en 	<= 1'b0;
 				end
 			end
 			WR_IDLE:
 				if (tx_ready == 0) begin
-					state <= WR_WAIT;
+					state 	<= WR_WAIT;
 					data_tx <= 8'd34;
 					out_en 	<= 1'b1;
 				end
 			WR_WAIT: begin 
-				wr_en <= 1'b0;
-				state <= WR_DONE;
+				wr_en 	<= 1'b0;
+				state 	<= WR_DONE;
 			end
 			WR_DONE:	begin
-				wr_en  <= 1'b1;
-				out_en <= 1'b0;
-				state <= RD_IDLE;
+				wr_en  	<= 1'b1;
+				out_en 	<= 1'b0;
+				state 	<= RD_IDLE;
 			end
 
 			default:
